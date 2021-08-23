f33baefd037d001b7e71b22f9e18e93886f14225
Toby Keegan <Toby.Keegan@ibm.com>
2021-08-23 08:42:59 +0100

    Add interactive console example

---
+++ samples/console.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Create a Unix-style interactive shell for MVS, SDSF
+# or subsystem commands. Adjust $ssid to handle prefixes.
+#
+# Usage: 
+#
+#     ./console.sh       - start an opercmd console
+#
+#     ./console.sh SSID  - start a console for a subsystem
+#
+# Example:
+#
+#     ./console.sh MQ01     
+#      !MQ01> START QMGR
+#
+# Copyright IBM Corp. 2021
+#
+
+if [ -z "$1" ]            
+then                      
+    prefix="opercmd"      
+else                      
+    prefix="$1"           
+    ssid="!$1"  # Replace prefix with your SSID prefix           
+fi                        
+                          
+while true                
+do                        
+    read -p "$prefix:> "  
+    opercmd ${ssid} $REPLY
+done         
