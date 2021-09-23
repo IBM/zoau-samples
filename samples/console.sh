47240a2cde76ecc371da8c57fcd7f787e1714010
Toby Keegan <Toby.Keegan@ibm.com>
2021-09-23 11:41:52 +0100

    format script to be tidy

--- samples/console.sh
+++ samples/console.sh
@@ -3,7 +3,7 @@
 # Create a Unix-style interactive shell for MVS, SDSF
 # or subsystem commands. Adjust $ssid to handle prefixes.
 #
-# Usage: 
+# Usage:
 #
 #     ./console.sh       - start an opercmd console
 #
@@ -11,22 +11,22 @@
 #
 # Example:
 #
-#     ./console.sh MQ01     
+#     ./console.sh MQ01
 #      !MQ01> START QMGR
 #
 # Copyright IBM Corp. 2021
 #
 
-if [ -z "$1" ]            
-then                      
-    prefix="opercmd"      
-else                      
-    prefix="$1"           
-    ssid="!$1"  # Replace prefix with your SSID prefix           
-fi                        
-                          
-while true                
-do                        
-    read -p "$prefix:> "  
-    opercmd ${ssid} $REPLY
-done         
+if [ -z "$1" ]
+then
+    prefix="opercmd"
+else
+    prefix="$1"
+    ssid="!$1"  # Replace prefix with your SSID prefix
+fi
+
+while true
+do
+    read -p "$prefix:> "
+    opercmd "${ssid} $REPLY"
+done
