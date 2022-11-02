8dd88c8738cc90ed802cf2fce1d57d60a40ac2e5
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-02 12:50:24 -0400

    Add iehlist.sh to samples.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

---
+++ samples/iehlist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+        cat << EOF
+usage:
+$0 <data set name>
+EOF
+        exit 1
+fi
+
+data_set=$1
+
+volume=$(dls -s ${data_set} | awk -F ' ' '{print $5}')
+
+mvscmd --pgm=IEHLIST \
+       --sysprint=stdout \
+       --dd1=${data_set},shr,volumes=${volume} \
+    --sysin=stdin <<zz
+ LISTPDS VOL=3390=${volume},FORMAT,                                       X
+               DSNAME=${data_set}
+zz
