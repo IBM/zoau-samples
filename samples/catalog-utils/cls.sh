402f58db2302be030c7e584277f02c1526ef69b1
Jesus Uribe <salvador.uribe@ibm.com>
2023-06-16 15:05:16 -0600

    update license text
    Signed-off-by: Jesus Uribe <salvador.uribe@ibm.com>

--- samples/catalog-utils/cls.sh
+++ samples/catalog-utils/cls.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-#*******************************************************************************
-# Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2019-2023. All Rights Reserved.
-#
-# Note to U.S. Government Users Restricted Rights:
-# Use, duplication or disclosure restricted by GSA ADP Schedule
-# Contract with IBM Corp.
-#*******************************************************************************
+# *******************************************************************************
+# 
+#  Copyright IBM Corp. 2023.
+# 
+#  Sample Material
+# 
+#  Licensee may copy and modify Source Components and Sample Materials for
+#  internal use only within the limits of the license rights under the Agreement
+#  for IBM Z Open Automation Utilities provided, however, that Licensee may not
+#  alter or delete any copyright information or notices contained in the Source
+#  Components or Sample Materials. IBM provides the Source Components and Sample
+#  Materials without obligation of support and "AS IS", WITH NO WARRANTY OF ANY
+#  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING THE WARRANTY OF TITLE,
+#  NON-INFRINGEMENT OR NON-INTERFERENCE AND THE IMPLIED WARRANTIES AND
+#  CONDITIONS OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# ******************************************************************************/
 # cls: list catalogs on the system
 #
 syntax() {
