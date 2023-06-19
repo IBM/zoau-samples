9b3e207b9707f0fd722bc0a77131937a63d580fc
Jesus Uribe <salvador.uribe@ibm.com>
2023-06-19 13:58:01 -0600

    Add license text
    Signed-off-by: Jesus Uribe <salvador.uribe@ibm.com>

--- samples/smpapply.sh
+++ samples/smpapply.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
+#*******************************************************************************
+#
+# Copyright IBM Corp. 2023.
+#
+# Sample Material
+#
+# Licensee may copy and modify Source Components and Sample Materials for
+# internal use only within the limits of the license rights under the Agreement
+# for IBM Z Open Automation Utilities provided, however, that Licensee may not
+# alter or delete any copyright information or notices contained in the Source
+# Components or Sample Materials. IBM provides the Source Components and Sample
+# Materials without obligation of support and "AS IS", WITH NO WARRANTY OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING THE WARRANTY OF TITLE,
+# NON-INFRINGEMENT OR NON-INTERFERENCE AND THE IMPLIED WARRANTIES AND
+# CONDITIONS OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+#******************************************************************************/
+#
 # smpapply - apply a PTF
 # smpapply <ptf> <zone> [<CSI-name>]
-#
-# Copyright IBM Corp. 2023
 #
 
 syntax() {
